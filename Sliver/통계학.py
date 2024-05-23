6c6622b877cff62423f942920ac5c9652f6b6d09
wngud1 <dlqns928@naver.com>
2024-05-23 18:31:37 +0900

    not Solve

--- Sliver/통계학.py
+++ Sliver/통계학.py
@@ -10,23 +10,20 @@ if len(cnt)==1:
     print(cnt[0])
 else:    
     print(cnt[int(len(cnt)//2)])
-
 for i in cnt:
-    if i in pocket:
-        pocket[i]+=1
+    count=1
+    if i in pocket.keys():
+        count+=1
+        pocket[i] = count
     else:
-        pocket[i]=1 
-mx=max(pocket.values())
-cnt2=[]
-for i in pocket:
-    if mx==pocket[i]:
-        cnt2.append(i)
-if len(cnt2)>1:
-    print(cnt2[1])
+        pocket[i]=1
+Max=max(pocket.values())
+print(pocket)
+if len(cnt)==1:
+    print(cnt[0])
+elif len(cnt)!=1 and Max==1:
+    print(cnt[1])
 else:
-    print(cnt2[0])           
-
-print(max(cnt)-min(cnt))
+    print()
 
 
-    
---
+++ 그룹단어체커.py
@@ -0,0 +1,29 @@
+"""내가 푼 코드
+n=int(input())
+cnt=[]
+count=0
+for i in range(n):
+    m=list(input())
+    cnt.append(m)
+for i in cnt:
+    x=0
+    if len(i)==1 or len(i)==2 or len(set(i))==1:
+        count+=1
+    elif i[x] not in i[x:] or i[x] not in i[x+1:]:
+                count+=1
+print(count)
+"""
+"""개선되누코드"""
+n = int(input())
+ans = 0
+for _ in range(n):
+    array = input()
+    for j in range(len(array)):
+        if j != len(array) - 1:
+            if array[j] == array[j + 1]:
+                continue
+            elif array[j] in array[j + 1:]:
+                break
+        else:
+            ans += 1
+print(ans)
