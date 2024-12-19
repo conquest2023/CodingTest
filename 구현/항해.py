4bfc4bef27888ce0f815cf6e3342eae745cca20b
counquest2023 <dlqns928@naver.com>
2024-12-20 00:00:40 +0900

    Solve

--- 구현/항해.py
+++ 구현/항해.py
@@ -3,17 +3,18 @@ N,X,Y=map(int,input().split())
 idx=list(map(int,input().split()))
 count=0
 ans=0
-idx.sort()
 for i in range(len(idx)):
     if idx[i] >= X:
-        if idx[i] <=Y:
+        if idx[i] <Y:
             ans += 1
         else:
-            if idx[i]%X<=Y-X+1:
-                 ans+=idx[i]//X
-            else:
-                 ans+=idx[i]//Y
-                 count+=idx[i]%Y
+             A=idx[i]//X
+             B=idx[i]//Y
+             if A>B:
+                ans+=A
+             else:
+                  ans+=B
+                  count+=idx[i]%Y
     else:
          count+=idx[i]
 print(ans)
